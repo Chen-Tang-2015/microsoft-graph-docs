83c9e8a7def06822c9e5435a2d8eb0ad26269adf
Chen-Tang-2015 <v-chenta@microsoft.com>
2016-01-14 12:47:08 -0800

    deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,49 +1,24 @@
- +@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
-
-:: ----------------------
-:: KUDU Deployment Script
-:: Version: 0.2.2
-:: ----------------------
-
-:: Prerequisites
-:: -------------
-
-:: Verify node.js installed
-where node 2>nul >nul
-IF %ERRORLEVEL% NEQ 0 (
-  echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
-  goto error
-)
-
 :: Setup
 :: -----
 
 setlocal enabledelayedexpansion
 
-SET ARTIFACTS=%~dp0%..
+SET SITE=%~dp0%..
 
-IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%.
-)
+SET DEPLOYMENT_SOURCE=%~dp0%.
 
-IF NOT DEFINED DEPLOYMENT_TARGET (
-  SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot\GraphDocuments
-)
+SET DEPLOYMENT_TARGET=%SITE%\wwwroot\GraphDocuments
 
-IF NOT DEFINED DEPLOYMENT_TEMPLATE (
-  SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot\MD\office-content-pr\rest-api\Microsoft.Graph\html-template
-)
+SET DEPLOYMENT_TEMPLATE=%SITE%\wwwroot\MD\office-content-pr\rest-api\Microsoft.Graph\html-template
 
-IF NOT DEFINED APIDOCS_PATH (
-  SET APIDOCS_PATH=%ARTIFACTS%\wwwroot\MD\apidocs\apidocs.exe
-)
+SET APIDOCS_PATH=%SITE%\wwwroot\MD\apidocs\apidocs.exe
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
 
 echo start to transfer md to html and copy to GraphDocuments folder
-mkdir %ARTIFACTS%\wwwroot\newdir
 
 call :ExecuteCmd %APIDOCS_PATH% publish --format mustache --path %DEPLOYMENT_SOURCE% --output %DEPLOYMENT_TARGET% --template %DEPLOYMENT_TEMPLATE%
 
