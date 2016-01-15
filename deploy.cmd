bb14da40fc700930ca6982f90c8fed72d55c53f6
Chen-Tang-2015 <v-chenta@microsoft.com>
2016-01-15 12:39:29 -0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -25,4 +25,4 @@ IF NOT DEFINED APIDOCS_PATH (
 
 %APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE% --output %DEPLOYMENT_TARGET% --template %DEPLOYMENT_TEMPLATE% --format mustache
 
-MOVE %DEPLOYMENT_TARGET%\*.css  %SITE%\GraphStyles /Y 
+MOVE \y  %SITE%/GraphDocuments/*.css  %SITE%/GraphStyles 
