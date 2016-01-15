7848abc7064c79a6355c879e1806b3915254a094
Chen-Tang-2015 <v-chenta@microsoft.com>
2016-01-15 12:49:49 -0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -3,25 +3,15 @@
 
 setlocal enabledelayedexpansion
 
-IF NOT DEFINED SITE (
-  SET SITE=%~dp0%..
-)
+SET SITE=%~dp0%..
 
-IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%SITE%\repository
-)
+SET DEPLOYMENT_SOURCE=%SITE%\repository
 
-IF NOT DEFINED DEPLOYMENT_TARGET (
-  SET DEPLOYMENT_TARGET=%SITE%\wwwroot\GraphDocuments
-)
+SET DEPLOYMENT_TARGET=%SITE%\wwwroot\GraphDocuments
 
-IF NOT DEFINED DEPLOYMENT_TEMPLATE (
-  SET DEPLOYMENT_TEMPLATE=%SITE%\wwwroot\MD\office-content-pr\rest-api\Microsoft.Graph\html-template
-)
+SET DEPLOYMENT_TEMPLATE=%SITE%\wwwroot\MD\office-content-pr\rest-api\Microsoft.Graph\html-template
 
-IF NOT DEFINED APIDOCS_PATH (
-  SET APIDOCS_PATH=%SITE%\wwwroot\MD\apidocs
-)
+SET APIDOCS_PATH=%SITE%\wwwroot\MD\apidocs
 
 %APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE% --output %DEPLOYMENT_TARGET% --template %DEPLOYMENT_TEMPLATE% --format mustache
 
